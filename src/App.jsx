28204b574bed7cb2fe669f3481e9cef491fe9dc6
David <idornigiedavid1@gmail.com>
2024-03-12 14:02:36 +0100

    Setting up development environment.

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Layout from './Components/Layout'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import Shop from './Pages/Shop'
+import Home from './Pages/Home'
+function App() {
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Layout/>}>
+            <Route index element={<Home/>}/>
+            <Route path='shop' element={<Shop/>}/>
+          </Route>
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
