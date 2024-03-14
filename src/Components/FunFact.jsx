6fbafc139e6ff9bcfdb0581a5a3d6e4d8ff964b0
David <idornigiedavid1@gmail.com>
2024-03-14 16:54:56 +0100

    Removed Unused files.

--- src/Components/FunFact.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FunFact = () => {
-  return (
-    <div>FunFact</div>
-  )
-}
-
-export default FunFact
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,12 @@
 import AboutSection from '../Components/About'
 import Header from '../Components/Header'
 import HeroSection from '../Components/Hero-section'
-import courseCategory from '../Components/courseCategory'
 
 const Home = () => {
   return (
     <>
       <HeroSection/>
       <AboutSection/>
-      <courseCategory/>
     </>
   )
 }
