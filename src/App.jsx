b57c1308c10ebaa7a9da0a2712121a0595516680
David <idornigiedavid1@gmail.com>
2024-05-07 00:57:45 +0100

    cleaned few codes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AuthProvider from "./Context/AuthContext";
 import PageNotFound from "./Pages/404Page";
 import Profile from "./Pages/UserProfile/Profile";
 import "./Responsive.css";
+import Checkout from "./Pages/Checkout";
 const Cart = React.lazy(() => import("./Pages/Cart/Cart"));
 const Contact = React.lazy(() => import("./Pages/Contact"));
 const Home = React.lazy(() => import("./Pages/Home"));
@@ -15,38 +16,55 @@ const ProductDetails = React.lazy(() => import("./Pages/ProductDetails"));
 const Shop = React.lazy(() => import("./Pages/Shop"));
 const Login = React.lazy(() => import("./Pages/User/User-Entry/Login"));
 const SignUp = React.lazy(() => import("./Pages/User/User-Entry/SignUp"));
-const UserAccount = React.lazy(() => import("./Pages/User/User-Entry/UserAccount"));
+const UserAccount = React.lazy(() =>
+  import("./Pages/User/User-Entry/UserAccount")
+);
+const ForgotPassword = React.lazy(() =>
+  import("./Pages/User/User-Entry/ForgotPassword")
+);
+
+const ResetPassword = React.lazy(() =>
+  import("./Pages/User/User-Entry/ResetPassword")
+);
+
+const UserBio = React.lazy(() =>
+  import("./Pages/User/User-Entry/UserBio")
+);
 
 const App = () => {
   return (
-    <div className="">
-        <Router>
-          <Suspense fallback={<Loading />}>
-            <AuthProvider>
-              <Routes>
-                {/* ENTRY PAGES */}
 
-                <Route path="login" element={<Login />} />
-                <Route path="register" element={<SignUp />} />
+      <Router>
+        <Suspense fallback={<Loading />}>
+          <AuthProvider>
+            <Routes>
+              {/* ENTRY PAGES */}
 
-                {/* END OF ENTRY PAGES */}
+              <Route path="login" element={<Login />} />
+              <Route path="register" element={<SignUp />} />
+              <Route path="forgot-password" element={<ForgotPassword />} />
+              <Route path="resetPassword/:token" element={<ResetPassword />} />
 
-                <Route path="/" element={<Layout />}>
-                  <Route path="profile" element={<Profile />} />
-                  <Route index element={<Home />} />
-                  <Route path="shop" element={<Shop />} />
-                  <Route path="product/:id" element={<ProductDetails />} />
-                  <Route path="cart" element={<Cart />} />
-                  <Route path="pricing" element={<Pricing />} />
-                  <Route path="contact" element={<Contact />} />
-                  <Route path="*" element={<PageNotFound />} />
-                  <Route path="account" element={<UserAccount />} />
-                </Route>
-              </Routes>
-            </AuthProvider>
-          </Suspense>
-        </Router>
-    </div>
+              {/* END OF ENTRY PAGES */}
+
+              <Route path="/" element={<Layout />}>
+                {/* <Route path="profile" element={<Profile />} /> */}
+                <Route index element={<Home />} />
+                <Route path="shop" element={<Shop />} />
+                <Route path="product/:id" element={<ProductDetails />} />
+                <Route path="cart" element={<Cart />} />
+                <Route path="checkout" element={<Checkout />} />
+                <Route path="pricing" element={<Pricing />} />
+                <Route path="contact" element={<Contact />} />
+                <Route path="mybio" element={<UserBio />} />
+                <Route path="profile" element={<UserAccount />} />
+                <Route path="*" element={<PageNotFound />} />
+              </Route>
+            </Routes>
+          </AuthProvider>
+        </Suspense>
+      </Router>
+
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import CartProvider from './Context/CartContext.jsx'
 import ProductProvider from './Context/ProductContext.jsx'
-import AuthProvider from './Context/AuthContext.jsx'
-// import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('raque')).render(
   <ProductProvider>
