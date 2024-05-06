b08d949c2aff9e079db2b0f91ee57719f103b593
David <idornigiedavid1@gmail.com>
2024-05-07 00:52:16 +0100

    fix(core): cleaned a few codes in the forgot-password file

---
+++ src/Pages/User/User-Entry/ForgotPassword.jsx
@@ -0,0 +1,66 @@
+import { Link } from "react-router-dom";
+import ForgotPasswordForm from "../../../Components/ForgotPasswordForm";
+import MainLogo from "../../../Components/MainLogo";
+import LoginPageImg from "../../../Components/LoginPageImg";
+
+
+const ForgotPassword = () => {
+  return (
+    <section className="forgotpassword-area">
+      <div className="row m-0 h-100">
+        {/* COL-LEFT */}
+        <div className="col-lg-6 col-md-12 p-0">
+          {/* REGISTER IMAGE & MAIN IMAGE */}
+          <div className="forgotpassword-image">
+            <LoginPageImg/>
+          </div>
+        </div>
+
+        {/* COL-RIGHT */}
+        <div className="col-lg-6 col-md-12 p-0">
+          {/* REGISTER CONTENT */}
+          <div className="forgotpassword-content">
+            <div className="d-table">
+              <div className="d-table-cell">
+                {/* REGUSTER FORM */}
+                <div className="forgotpassword-form">
+                  <div className="logo">
+                    <Link to="/">
+                      <MainLogo />
+                    </Link>
+
+                    <Link to="/" className="d-none">
+                      <img
+                        src="assets/img/logo.png"
+                        className="white-logo"
+                        alt="image"
+                      />
+                    </Link>
+                  </div>
+
+                  {/* REGISTER TITLE */}
+                  <h3>Validate Email</h3>
+
+                  <p>
+                    Forgot your password?&nbsp;{" "}
+                    <span className="text-deepred">
+                      Enter your email below👇
+                    </span>
+                  </p>
+
+                  {/* FORM */}
+                  <ForgotPasswordForm />
+
+                  {/* MAIN FORM END */}
+                </div>
+                {/* REGISTER FORM END */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ForgotPassword;
