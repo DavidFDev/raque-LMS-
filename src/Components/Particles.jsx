5c67e2f25ccb390e59053625b26d3bcf417457b1
David <idornigiedavid1@gmail.com>
2024-05-07 00:53:25 +0100

    created a component for tsparticles in order to use it

---
+++ src/Components/Particles.jsx
@@ -0,0 +1,143 @@
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { useEffect, useMemo, useState } from "react";
+// import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
+// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
+
+const ParticlesComponent = (props) => {
+  const [init, setInit] = useState(false);
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadSlim(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  const options = useMemo(
+    () => ({
+      fullScreen: {
+        enable: false,
+        zIndex: -0,
+      },
+      particles: {
+        number: {
+          value: 150,
+          density: {
+            enable: true,
+            area: 800,
+          },
+        },
+        color: {
+          value: ["#BD10E0", "#FF1949", "#50E3C2", "#FFD300", "#E86363"],
+        },
+        shape: {
+          type: "circle",
+          stroke: {
+            width: 0,
+            color: "#b6b2b2",
+          },
+        },
+        opacity: {
+          value: 0.5211089197812949,
+          random: false,
+          animation: {
+            enable: true,
+            speed: 1,
+            minimumValue: 0.1,
+            sync: false,
+          },
+        },
+        size: {
+          value: 2.7777,
+          random: true,
+          animation: {
+            enable: true,
+            speed: 12.181158184520175,
+            minimumValue: 0.1,
+            sync: false,
+          },
+        },
+        lineLinked: {
+          enable: false,
+          distance: 150,
+          color: "#c8c8c8",
+          opacity: 0.4,
+          width: 1,
+        },
+        move: {
+          enable: true,
+          speed: 1,
+          direction: "none",
+          random: false,
+          straight: false,
+          outMode: "bounce",
+          bounce: false,
+          attract: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 1200,
+          },
+        },
+      },
+      interactivity: {
+        detectOn: "canvas",
+        events: {
+          onHover: {
+            enable: true,
+            mode: "connect",
+          },
+          onClick: {
+            enable: false,
+            mode: "push",
+          },
+          resize: true,
+        },
+        modes: {
+          grab: {
+            distance: 400,
+            lineLinked: {
+              opacity: 1,
+            },
+          },
+          bubble: {
+            distance: 400,
+            size: 40,
+            duration: 2,
+            opacity: 8,
+            speed: 3,
+          },
+          connect: {},
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+          push: {
+            particles_nb: 4,
+          },
+          remove: {
+            particles_nb: 2,
+          },
+        },
+      },
+      detectRetina: true,
+    }),
+    []
+  );
+
+  return <Particles id={props.id} init={particlesLoaded} options={options} />;
+};
+
+export default ParticlesComponent;
